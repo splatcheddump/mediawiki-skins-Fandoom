d321a4533df47f83666c7365de14fc32581c501b
whostacking <lostfromthelabyrinth@gmail.com>
2026-07-05 18:07:57 +0800

    test: 🚨 testing something

--- resources/skins.citizen.scripts/setupObservers.js
+++ resources/skins.citizen.scripts/setupObservers.js
@@ -305,16 +305,16 @@ const init = ( { document, window, mw, IntersectionObserver } ) => {
 	};
 
 	const scrollObserver = createScrollObserver( { IntersectionObserver } );
-	const pageHeaderObserver = scrollObserver.observe(
-		() => {
-			document.body.classList.add( PAGE_TITLE_INTERSECTION_CLASS );
-			resumeStickyHeader();
-		},
-		() => {
-			document.body.classList.remove( PAGE_TITLE_INTERSECTION_CLASS );
-			pauseStickyHeader();
-		}
-	);
+    const pageHeaderObserver = scrollObserver.observe(
+        () => {
+            document.body.classList.remove( PAGE_TITLE_INTERSECTION_CLASS );
+            pauseStickyHeader();
+        },
+        () => {
+            document.body.classList.add( PAGE_TITLE_INTERSECTION_CLASS );
+            resumeStickyHeader();
+        }
+    );
 
 	pageHeaderObserver.observe( stickyIntersection );
 
